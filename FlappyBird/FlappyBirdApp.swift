d5a5f68f588059e84de2717b8bad4356903d9651
ILIA <dubenskiiilia@gmail.com>
2023-11-06 20:07:46 +0300

    Initial Commit

---
+++ FlappyBird/FlappyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyBirdApp.swift
+//  FlappyBird
+//
+//  Created by Илья Дубенский on 06.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlappyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
